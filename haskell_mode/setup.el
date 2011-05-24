0dcdc09a00c61c9322f39ea0270db1c5555b878d
TsuruokaTakashi <tsuruoka@newSpice.local>
2011-05-24 18:14:16 +0900

    add haskel-mode from [Chttp://projects.haskell.org/haskellmode-emacs/

---
+++ haskell_mode/setup.el
@@ -0,0 +1,5 @@
+(load "~/lib/emacs/haskell-mode/haskell-site-file")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'font-lock-mode)
+;;(setq haskell-program-name "/usr/local/bin/ghci")
