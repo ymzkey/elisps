14701644556924258fb9343200b567ccc6d76126
TsuruokaTakashi <tsuruoka@newSpice.local>
2011-05-24 18:23:35 +0900

    oops

--- haskell_mode/setup.el
+++ haskell_mode/setup.el
@@ -1,4 +1,4 @@
-(load "~/lib/emacs/haskell-mode/haskell-site-file")
+(load "./haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'font-lock-mode)
