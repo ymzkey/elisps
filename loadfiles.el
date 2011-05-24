6b97866bded0c0d0a9f2c2328d279af8cd577476
tsuruoka <tsuruoka@exspice.local>
2011-05-24 22:21:59 +0900

    auto load each projects

--- loadfiles.el
+++ loadfiles.el
@@ -1,21 +1,31 @@
-(setq projects
-      `(
-        "ruby_mode" 
-	"haskell_mode"
-        )
-      )
+;;all projects are in each folder and each setup.el most exist
+;;get projects list
+(progn
+  (setq projects (file-name-all-completions "" "~/elisps"))
+  (setq files (directory-files "~/elisps"))
+  
+  (setq projects (remove "./" projects))
+  (setq projects (remove "../" projects))
+  (setq projects (remove ".git/" projects))
+  (setq projects (remove "" projects))
 
+  (while files
+    (setq projects (remove (car files) projects))
+    (setq files (cdr files))
+    )
+  )
+
+;;add load-path
+;;load setup.el
 (while projects
   (setq path_project (concat "~/elisps/" (car projects)))
-  ;;add load-path
   (setq load-path
 	(append
 	 (list
 	  (expand-file-name path_project)
 	  )
 	 load-path))
-  ;;load setup.el
-  (load (concat (concat path_project "/") "setup.el"))
+  (load (concat path_project "setup.el"))
   ;;next
   (setq projects (cdr projects))
   )
